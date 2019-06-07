1c5e829fcdd3433cdb5ae0019da1b5750195b120
Nastya Faizulina <just-fy@mail.ru>
2019-06-07 11:30:05 +0300

    add options for --help

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,5 +1,18 @@
-const testFunc = () => {
-    console.log('Hello, world');
-};
+import program from 'commander';
+import { version } from '../../package.json';
 
-export default testFunc;
+program
+    .version(version)
+    .description('Compares two configuration files and shows a difference')
+    .option('-h, --help', 'enable some foo')
+    .option('-V, --version', 'output the version number');
+
+program.on('--help', function(){
+    console.log(program.description);
+    console.log('Options:');
+    console.log(program.opts());
+});
+
+program.parse(process.argv);
+
+program.help();
