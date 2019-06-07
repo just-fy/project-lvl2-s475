4a22874b3da7287cdcf83afd5281a2a6fb812eff
Nastya Faizulina <just-fy@mail.ru>
2019-06-07 13:00:17 +0300

    test changes

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,16 +3,18 @@ import { version } from '../../package.json';
 
 program
     .version(version)
-    .description('Compares two configuration files and shows a difference')
-    .option('-h, --help', 'enable some foo')
-    .option('-V, --version', 'output the version number');
+	.arguments('<firstConfig> <secondConfig>')
+    .description('Compares two configuration files and shows a difference.')
+    // .option('-h, --help', 'output usage information')
+    // .option('-V, --version', 'output the version number')
+    .option('-f, --format [type]', 'Output format')
+	.parse(process.argv);
 
-program.on('--help', function(){
-    console.log(program.description);
-    console.log('Options:');
-    console.log(program.opts());
-});
+// program.on('--help', function(){
+//     console.log(program.description);
+//     console.log('Options:');
+//     console.log(program.opts());
+// });
+//
 
-program.parse(process.argv);
-
-program.help();
+if (!program.args.length) program.help();
