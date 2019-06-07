b5612bf3117028f31331e3e9c68f97c3a0b061ce
Nastya Faizulina <just-fy@mail.ru>
2019-06-07 16:31:21 +0300

    test action, not working

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,14 +2,24 @@ import program from 'commander';
 import { version } from '../../package.json';
 
 program
-    .version(version)
 	.arguments('<firstConfig> <secondConfig>')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'Output format')
+	.description('Compares two configuration files and shows a difference.')
+	.version(version)
+	.option('-f, --format [type]', 'Output format')
+	.action(function (firstConfig, secondConfig) {
+		console.log('hey');
+	})
 	.parse(process.argv);
 
-if (!program.args.length) program.help();
-
-program.on('--help', function(){
-    console.log('hello');
-});
+// program
+//     .version(version)
+// 	.arguments('<firstConfig> <secondConfig>')
+//     .description('Compares two configuration files and shows a difference.')
+//     .option('-f, --format [type]', 'Output format')
+// 	.parse(process.argv);
+//
+// if (!program.args.length) program.help();
+//
+// program.on('--help', function(){
+//     console.log('hello');
+// });
