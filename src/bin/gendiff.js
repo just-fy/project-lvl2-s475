eb3ec7ae11d4f6db1cb8967dadf11497c88f18ac
Nastya Faizulina <just-fy@mail.ru>
2019-06-09 20:36:33 +0300

    change gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,28 +1,17 @@
 import program from 'commander';
 import { version } from '../../package.json';
 
-// program
-// 	.arguments('<firstConfig> <secondConfig>')
-// 	.description('Compares two configuration files and shows a difference.')
-// 	.version(version)
-// 	.option('-f, --format [type]', 'Output format')
-// 	.action(function (firstConfig, secondConfig) {
-// 		console.log('hey');
-// 	})
-// 	.parse(process.argv);
-
 program
-    .version(version)
-	.arguments('<firstConfig>')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'Output format')
-	.action((firstConfig) => {
-		firstConfigValue = firstConfig;
-	});
+	.version(version)
+	.arguments('<firstConfig> <secondConfig>')
+	.description('Compares two configuration files and shows a difference.')
+	.option('-f, --format [type]', 'Output format');
 
-	program.parse(process.argv);
-if (typeof firstConfigValue === 'undefined') {
-	console.error('no command given!');
-	process.exit(1);
-}
-console.log('command: ', firstConfigValue);
+program.on('--help', function(){
+	console.log('Hello');
+});
+
+program.parse(process.argv);
+
+program.help();
+
