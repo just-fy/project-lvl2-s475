4f789d2df4044681ae32549052c419af24fe845e
Nastya Faizulina <just-fy@mail.ru>
2019-06-09 18:48:30 +0300

    test

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,25 +1,28 @@
 import program from 'commander';
 import { version } from '../../package.json';
 
-program
-	.arguments('<firstConfig> <secondConfig>')
-	.description('Compares two configuration files and shows a difference.')
-	.version(version)
-	.option('-f, --format [type]', 'Output format')
-	.action(function (firstConfig, secondConfig) {
-		console.log('hey');
-	})
-	.parse(process.argv);
-
 // program
-//     .version(version)
 // 	.arguments('<firstConfig> <secondConfig>')
-//     .description('Compares two configuration files and shows a difference.')
-//     .option('-f, --format [type]', 'Output format')
+// 	.description('Compares two configuration files and shows a difference.')
+// 	.version(version)
+// 	.option('-f, --format [type]', 'Output format')
+// 	.action(function (firstConfig, secondConfig) {
+// 		console.log('hey');
+// 	})
 // 	.parse(process.argv);
-//
-// if (!program.args.length) program.help();
-//
-// program.on('--help', function(){
-//     console.log('hello');
-// });
+
+program
+    .version(version)
+	.arguments('<firstConfig>')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'Output format')
+	.action((firstConfig) => {
+		firstConfigValue = firstConfig;
+	});
+
+	program.parse(process.argv);
+if (typeof firstConfigValue === 'undefined') {
+	console.error('no command given!');
+	process.exit(1);
+}
+console.log('command: ', firstConfigValue);
