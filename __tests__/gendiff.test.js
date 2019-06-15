a19e5d3d2efb0e62f59add0a5b256a853713b468
Nastya Faizulina <just-fy@mail.ru>
2019-06-15 21:00:06 +0300

    add test file for gendiff

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,12 @@
+import gendiff from '../src/bin/gendiff';
+
+describe('gendiffTest', () => {})
+
+// Usage: gendiff [options] <firstConfig> <secondConfig>
+//
+// This utility compares two configuration files and shows a difference.
+//
+// 	Options:
+// -V, --version        output the version number
+// -f, --format [type]  Output format
+// -h, --help           output usage information
